3871e3edffdaaf0c35a9bba8ddf0d5d63dbd7d37
Aditya Dey <deyaditya34@gmail.com>
2023-07-25 01:27:26 +0530

    formatted

--- src/middlewares/params-validator.js
+++ src/middlewares/params-validator.js
@@ -2,9 +2,10 @@ const httpError = require("http-errors");
 
 const createParamValidator = (params, paramsKey) => (req, res, next) => {
   const reqParams = Reflect.get(req, paramsKey);
+  
   const missingParams = params.filter(
     (param) => !Reflect.has(reqParams, param)
-    );
+  );
 
   if (missingParams.length > 0) {
     throw httpError.BadRequest(
@@ -18,11 +19,11 @@ const createParamValidator = (params, paramsKey) => (req, res, next) => {
 };
 
 const PARAM_KEY = {
-    BODY: "body",
-    QUERY: "query"
+  BODY: "body",
+  QUERY: "query",
 };
 
 module.exports = {
-    createParamValidator,
-    PARAM_KEY
-}
+  createParamValidator,
+  PARAM_KEY,
+};
