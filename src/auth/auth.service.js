b61be1396a3d98d53843fa8c2d9002bbd9d6d44e
Aditya Dey <deyaditya34@gmail.com>
2023-07-31 19:57:38 +0530

    register fn code updated

--- src/auth/auth.service.js
+++ src/auth/auth.service.js
@@ -1,84 +1,67 @@
-const {scryptSync} = require("crypto");
 const httpError = require("http-errors");
 const database = require("../services/database.service");
 const jwtService = require("../services/jwt.service");
-const {COLLECTION_NAMES, PASSWORD_SALT} = require("../config");
-
+const { COLLECTION_NAMES } = require("../config");
+const { buildUser, encryptPassword } = require("./auth.utils");
 
 async function register(username, password) {
-    const existingUser = await database
-        .getCollection(COLLECTION_NAMES.USERS)
-        .findOne({
-            username
-        })
-    /**
-     * @fix remove console logging
-     */
-    console.log("existing user", existingUser);    
-    if (existingUser) {
-        throw new httpError.UnprocessableEntity(
-            `Username '${username}' is already taken`
-        )
-    }
+  const existingUser = await database
+    .getCollection(COLLECTION_NAMES.USERS)
+    .findOne({
+      username: username,
+    });
 
-    /**
-     * @fix use a method like `buildUser` to build the record first and 
-     *      then pass it to `insertOne()`
-     */
-    await database.getCollection(COLLECTION_NAMES.USERS).insertOne({
-        username,
-        password: encryptPassword(password),
-        role: "USER"
-    })
-}
+  if (existingUser) {
+    throw new httpError.UnprocessableEntity(
+      `Username '${username}' is already taken`
+    );
+  }
 
-function encryptPassword(password) {
-    return scryptSync(password, PASSWORD_SALT, 64).toString("hex");
+  const userDetails = buildUser(username, password);
+
+  await database.getCollection(COLLECTION_NAMES.USERS).insertOne({userDetails});
 }
 
 async function login(username, password) {
-    const user = await database.getCollection(COLLECTION_NAMES.USERS).findOne({
-        username,
-        password: encryptPassword(password)
-    })
+  const user = await database.getCollection(COLLECTION_NAMES.USERS).findOne({
+    username,
+    password: encryptPassword(password),
+  });
 
-    if (!user) {
-        throw new httpError.Unauthorized("Username/Password combo incorrect")
-    }
+  if (!user) {
+    throw new httpError.Unauthorized("Username/Password combo incorrect");
+  }
 
-    const token = jwtService.createToken({username});
+  const token = jwtService.createToken({ username });
 
-    return token;
+  return token;
 }
 
 async function getUserFromToken(token) {
-    const payload = jwtService.decodeToken(token);
+  const payload = jwtService.decodeToken(token);
 
-    if(!payload) {
-        return null;
-    }
+  if (!payload) {
+    return null;
+  }
 
-    const username = payload.username;
-    const user = await database.getCollection(COLLECTION_NAMES.USERS)
-    .findOne({username}, {projection : {_id: false, password: false}});
+  const username = payload.username;
+  const user = await database
+    .getCollection(COLLECTION_NAMES.USERS)
+    .findOne({ username }, { projection: { _id: false, password: false } });
 
-    return user;
+  return user;
 }
 
-function getAllUsers() {
-    return database.getCollection(COLLECTION_NAMES.USERS)
-    /**
-     * @fix why is the name `getAllUsers` if the method is not actually getting ALL users
-     * 
-     * move the query part to controller. maybe rename this method to `queryUsers()`
-     */
-    .find({ role: {$ne : "ADMIN"}})
-    .toArray()
+async function findUsers(criteria) {
+  return database
+    .getCollection(COLLECTION_NAMES.USERS)
+    .find(criteria)
+    .toArray();
 }
 
 module.exports = {
-    register,
-    login,
-    getUserFromToken,
-    getAllUsers
-}
+  register,
+  login,
+  getUserFromToken,
+  findUsers,
+};
