f4b6269825feadcb5d6c4801b424cfc0f4a242c4
Aditya Dey <deyaditya34@gmail.com>
2023-07-31 19:53:40 +0530

    http error message updated and code optimized

--- src/middlewares/user-resolver.js
+++ src/middlewares/user-resolver.js
@@ -1,13 +1,13 @@
 const httpError = require("http-errors");
 const {AUTH_TOKEN_HEADER_FIELD} = require("../config");
-const {getUserFromToken} = require("../authorisation/auth.service");
+const {getUserFromToken} = require("../auth/auth.service");
 
 
 async function userResolver(req, res, next) {
   const token = Reflect.get(req.headers, AUTH_TOKEN_HEADER_FIELD);
 
   if(!token) {
-    throw new httpError.Forbidden("Access Denied. Please provide the relevant Token");
+    throw new httpError.Forbidden("Access Denied");
   }
 
   const user = await getUserFromToken(token);
@@ -15,7 +15,7 @@ async function userResolver(req, res, next) {
   if(!user) {
     throw new httpError.Forbidden("Invalid Token");
   }
-  console.log("User is", user);
+
   Reflect.set(req.body, "user", user);
   
   next();
