2b12cec4a2e00ea1270daebafe81798d0f86604a
Aditya Dey <deyaditya34@gmail.com>
2023-07-31 19:56:01 +0530

    route updated

--- src/auth/auth.api.router.js
+++ src/auth/auth.api.router.js
@@ -3,12 +3,12 @@ const express = require("express");
 
 const loginUser = require("./auth.login.api");
 const registerUser = require("./auth.register.api");
-const getAllUsers = require("./get-all-users.api")
+const queryUsers = require("./query-users.api")
 
 const router = express.Router();
 
 router.post("/login", loginUser);
 router.post("/register", registerUser);
-router.get("/", getAllUsers);
+router.get("/", queryUsers);
 
 module.exports = router;
