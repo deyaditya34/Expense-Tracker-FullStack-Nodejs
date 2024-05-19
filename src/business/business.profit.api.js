410d05958cdc329ea3235277f88b3c672872e062
aditya dey <deyaditya34@gmail.com>
2024-05-19 22:24:53 +0530

    feat added: business profit api

---
+++ src/business/business.profit.api.js
@@ -0,0 +1,50 @@
+const httpError = require("http-errors");
+
+const buildApiHandler = require("../api-utils/build-api-handler");
+const paramsValidator = require("../middlewares/params-validator");
+const userResolver = require("../middlewares/user-resolver");
+const businessService = require("./business.service");
+
+async function controller(req, res) {
+    const {dateTo, dateFrom} = req.query;
+
+    
+}
+
+const missingParamsValidator = paramsValidator.createParamValidator(
+  ["dateTo", "dateFrom"],
+  paramsValidator.PARAM_KEY.QUERY
+);
+
+function validateParams(req, res, next) {
+    const { dateTo, dateFrom } = req.query;
+  
+    if (typeof dateTo !== "string") {
+      throw new httpError.BadRequest(
+        `Field 'dateTo' - '${dateTo}' should be 'string' type.`
+      );
+    } else if (!new Date(dateTo).valueOf() > 0) {
+      throw new httpError.BadRequest(
+        `invalid 'dateTo' - '${dateTo}', should be either in format 'year-month-day' or 'month-day-year' `
+      );
+    }
+  
+    if (typeof dateFrom !== "string") {
+      throw new httpError.BadRequest(
+        `Field 'dateFrom' - '${dateFrom}' should be 'string' type.`
+      );
+    } else if (!new Date(dateFrom).valueOf() > 0) {
+      throw new httpError.BadRequest(
+        `invalid 'dateFrom' - '${dateFrom}', should be either in format 'year-month-day' or 'month-day-year' `
+      );
+    }
+  
+    next();
+  }
+
+module.exports = buildApiHandler([
+  userResolver,
+  missingParamsValidator,
+  validateParams,
+  controller,
+]);
